d63c55f1d913821495d5f25cb00e1a699adabcb6
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2024-01-28 19:07:13 -0500

    Create data_receiver.py

---
+++ data_receiver.py
@@ -0,0 +1,24 @@
+import socket
+
+# Configuración del servidor
+
+HOST = '0.0.0.0'  # Todas las interfaces disponibles
+PUERTO = 80  # Puerto HTTP
+
+# Inicio del servidor
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PUERTO))
+    s.listen()
+    print(f"SRecibiendo en {HOST}:{PUERTO}")
+
+    # Esperar la conexión del cliente
+    conn, addr = s.accept()
+    with conn:
+        print(f"Conexión establecida desde {addr}")
+        while True:
+            # Recibir los datos del cliente
+            data = conn.recv(1024)
+            if not data:
+                break
+            # Datos recibidos
+            print(f"Datos recibidos: {data.decode()}")
